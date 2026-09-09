3a9e93ef2d71ba0e54367de976587d54c8a901f2
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 21:52:14 +0530

    Day 1: Complete Rotate Array logic

--- Day01/RotateArray.java
+++ Day01/RotateArray.java
@@ -4,7 +4,10 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k %= n;
+
         reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 
     private void reverse(int[] nums, int start, int end) {
