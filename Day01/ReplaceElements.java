8e8fe16ed3c8d3eea86a1d77125f087ceba52a27
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 22:19:15 +0530

    Day 1: Replace elements and update maximum

--- Day01/ReplaceElements.java
+++ Day01/ReplaceElements.java
@@ -5,6 +5,12 @@ class Solution {
         
         for (int i = arr.length - 1; i >= 0; i--) {
             int temp = arr[i];
+            
+            arr[i] = max;
+            
+            if (temp > max) {
+                max = temp;
+            }
         }
         
         return arr;
