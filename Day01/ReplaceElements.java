089d2ff1e7398604f7e7b28e9109aa8c30a41ede
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 22:19:43 +0530

    Day 1: Complete Replace Elements solution

--- Day01/ReplaceElements.java
+++ Day01/ReplaceElements.java
@@ -6,6 +6,7 @@ class Solution {
         for (int i = arr.length - 1; i >= 0; i--) {
             int temp = arr[i];
             
+            // Replace current element with greatest element on right
             arr[i] = max;
             
             if (temp > max) {
