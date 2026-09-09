64bd960b43f4c642c5703f12657a30c149ee073a
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 21:51:04 +0530

    Day 1: Implement array reverse logic

--- Day01/RotateArray.java
+++ Day01/RotateArray.java
@@ -7,6 +7,12 @@ class Solution {
     }
 
     private void reverse(int[] nums, int start, int end) {
-        
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
