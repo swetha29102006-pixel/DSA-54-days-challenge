7a90a801cf9231ac99fcda540a1edb5c17329d74
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 22:13:58 +0530

    Day 1: Duplicate zeros and adjust index

--- Day01/DuplicateZeros.java
+++ Day01/DuplicateZeros.java
@@ -7,6 +7,9 @@ class Solution {
                 for (int j = length - 1; j > i + 1; j--) {
                     arr[j] = arr[j - 1];
                 }
+
+                arr[i + 1] = 0;
+                i++;
             }
         }
     }
