0abf1758e4ad208e1f4bf89e797c5877a492797a
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 21:55:52 +0530

    Day 1: Add Binary Search loop

--- Day01/BinarySearch.java
+++ Day01/BinarySearch.java
@@ -2,5 +2,9 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+
+        while (left <= right) {
+            
+        }
     }
 }
