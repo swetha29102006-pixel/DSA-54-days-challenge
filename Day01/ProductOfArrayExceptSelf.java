cd111ce41dd29882f67f7cf5bf1aa7b4dfe3673d
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 22:04:09 +0530

    Day 1: Calculate prefix products

--- Day01/ProductOfArrayExceptSelf.java
+++ Day01/ProductOfArrayExceptSelf.java
@@ -1,6 +1,14 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+
         int n = nums.length;
         int[] res = new int[n];
+
+        int pre = 1;
+
+        for(int i = 0; i < n; i++){
+            res[i] = pre;
+            pre *= nums[i];
+        }
     }
 }
