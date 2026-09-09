73cb0e223e48863a40e4a0fc19f0fb90e8a6765e
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 22:08:02 +0530

    Day 1: Maintain decreasing deque for sliding window

--- Day01/SlidingWindowMaximum.java
+++ Day01/SlidingWindowMaximum.java
@@ -4,5 +4,13 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> q = new ArrayDeque<>();
         List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            while (!q.isEmpty() && nums[q.getLast()] <= nums[i]) {
+                q.removeLast();
+            }
+
+            q.addLast(i);
+        }
     }
 }
