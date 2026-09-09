838e989220667037d586fe2efbc5918873ed2fb3
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 21:59:06 +0530

    Day 1: Add left half search condition

--- Day01/BinarySearch.java
+++ Day01/BinarySearch.java
@@ -5,12 +5,14 @@ class Solution {
 
         while (left <= right) {
             int mid = (left + right) / 2;
+
             if (nums[mid] == target) {
-        return mid;
-    }
-            else if (nums[mid] < target) {
-    left = mid + 1;
-}
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
     }
 }
