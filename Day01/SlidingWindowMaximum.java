8f7a2c948ae0b38c4f57041a0fcd8e0d77e0adce
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 22:07:27 +0530

    Day 1: Initialize Sliding Window Maximum

---
+++ Day01/SlidingWindowMaximum.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> q = new ArrayDeque<>();
+        List<Integer> res = new ArrayList<>();
+    }
+}
