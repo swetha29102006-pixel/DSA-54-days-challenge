71c157a91b64abd072ea692b88b26446887d7645
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-09 21:53:01 +0530

    Day 1: Finalize Rotate Array solution

--- Day01/RotateArray.java
+++ Day01/RotateArray.java
@@ -5,6 +5,7 @@ class Solution {
         int n = nums.length;
         k %= n;
 
+        // Rotate using three reversals
         reverse(nums, 0, n - 1);
         reverse(nums, 0, k - 1);
         reverse(nums, k, n - 1);
