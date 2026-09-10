419f5703c8014265196de53108ed086456035636
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 18:53:37 +0530

    Update CountSmallerNumbersAfterSelf.java

--- Day02/CountSmallerNumbersAfterSelf.java
+++ Day02/CountSmallerNumbersAfterSelf.java
@@ -1,6 +1,13 @@
 class Solution {
     int[] bit = new int[20002];
 
+    void add(int i) {
+        while (i < bit.length) {
+            bit[i]++;
+            i += i & -i;
+        }
+    }
+
     public List<Integer> countSmaller(int[] nums) {
         int n = nums.length;
         Integer[] ans = new Integer[n];
