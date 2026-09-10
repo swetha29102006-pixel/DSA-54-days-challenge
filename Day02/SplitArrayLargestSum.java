2e0fe2e0260fe662b61fe53c63dc6ca00a1c8981
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 18:35:45 +0530

    Calculate minimum and maximum range

--- Day02/SplitArrayLargestSum.java
+++ Day02/SplitArrayLargestSum.java
@@ -2,6 +2,11 @@ class Solution {
     public int splitArray(int[] nums, int k) {
         int low = 0, high = 0;
         
+        for (int i : nums) {
+            high += i;
+            low = Math.max(i, low);
+        }
+        
         return 0;
     }
 }
