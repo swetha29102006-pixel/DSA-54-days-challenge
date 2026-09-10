4e0e4159c6c67be1d7a486f7426993f6a606f835
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 18:57:40 +0530

    Update SortAnArray.java

--- Day02/SortAnArray.java
+++ Day02/SortAnArray.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
     }
 }
