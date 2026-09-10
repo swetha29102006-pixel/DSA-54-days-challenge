5e552e838839e3dddffdd3940ba2dafd8bc85c28
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 18:39:22 +0530

    Clean up and finalize Split Array solution

--- Day02/SplitArrayLargestSum.java
+++ Day02/SplitArrayLargestSum.java
@@ -1,22 +1,23 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
         int low = 0, high = 0;
-        
+
         for (int i : nums) {
             high += i;
             low = Math.max(i, low);
         }
-        
+
         return find(low, high, nums, k);
     }
-    
+
     private int find(int l, int h, int n[], int k) {
-        if (l > h) return l;
-        
+        if (l > h)
+            return l;
+
         int c = 1;
         int m = l + (h - l) / 2;
         int sum = 0;
-        
+
         for (int i : n) {
             if (i + sum > m) {
                 sum = i;
@@ -25,10 +26,10 @@ class Solution {
                 sum += i;
             }
         }
-        
+
         if (c <= k)
             return find(l, m - 1, n, k);
-        
+
         return find(m + 1, h, n, k);
     }
 }
