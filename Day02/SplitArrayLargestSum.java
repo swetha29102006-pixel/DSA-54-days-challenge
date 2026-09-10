1985297a2be78ba558acfbfe1ab17c1b1c7c7178
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 18:36:27 +0530

    Update SplitArrayLargestSum.java

--- Day02/SplitArrayLargestSum.java
+++ Day02/SplitArrayLargestSum.java
@@ -7,6 +7,12 @@ class Solution {
             low = Math.max(i, low);
         }
         
+        return find(low, high, nums, k);
+    }
+    
+    private int find(int l, int h, int n[], int k) {
+        if (l > h) return l;
+        
         return 0;
     }
 }
