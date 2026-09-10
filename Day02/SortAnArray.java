60cd7bbc59089468aa88ebac82019f023d99ca8d
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 18:57:56 +0530

    Update SortAnArray.java

--- Day02/SortAnArray.java
+++ Day02/SortAnArray.java
@@ -5,5 +5,11 @@ class Solution {
     }
 
     public static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int middle = (high - low) / 2 + low;
+
+            mergeSort(arr, low, middle);
+            mergeSort(arr, middle + 1, high);
+        }
     }
 }
