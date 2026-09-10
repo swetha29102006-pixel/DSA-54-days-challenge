3294b8fa17a174e0fc92a0c0cb389fc798e53723
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 19:16:14 +0530

    Handle boundary cases and complete solution

--- Day02/PeakIndexInMountainArray.java
+++ Day02/PeakIndexInMountainArray.java
@@ -15,12 +15,17 @@ class Solution
             {
                 if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
                     return mid;
-
+                
                 else if (arr[mid] < arr[mid + 1])
                     low = mid + 1;
                 else
                     high = mid - 1;
             }
+
+            if (mid == len - 1)
+                high = mid - 1;
+            else if (mid == 0)
+                low = mid + 1;
         }
 
         return 1;
