be683d45b06f27f92df32036657db32db99188be
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 18:42:26 +0530

    Update FindKthSmallestPairDistance.java

--- Day02/FindKthSmallestPairDistance.java
+++ Day02/FindKthSmallestPairDistance.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 class Solution {
     public int smallestDistancePair(int[] numbers, int k) {
         Arrays.sort(numbers);
+
+        int minDistance = 0;
+        int maxDistance = numbers[numbers.length - 1] - numbers[0];
+
         return 0;
     }
 }
