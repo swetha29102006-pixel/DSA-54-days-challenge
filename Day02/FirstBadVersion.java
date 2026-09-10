4bf124c2997b3188fde2e2c689186027535f0fd7
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-10 19:02:15 +0530

    Update FirstBadVersion.java

--- Day02/FirstBadVersion.java
+++ Day02/FirstBadVersion.java
@@ -2,6 +2,9 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         if (n == 1) return 1;
 
+        int l = 0;
+        int r = n;
+
         return 1;
     }
 }
