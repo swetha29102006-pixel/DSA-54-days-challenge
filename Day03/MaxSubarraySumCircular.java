d14d774610114eb4f126a22826a523ef3f008b93
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:28:05 +0530

    Update MaxSubarraySumCircular.java

--- Day03/MaxSubarraySumCircular.java
+++ Day03/MaxSubarraySumCircular.java
@@ -1,5 +1,10 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
+        int currMin = nums[0];
+        int currMax = nums[0];
+        int minSum = nums[0];
+        int maxSum = nums[0];
+
         return 0;
     }
 }
