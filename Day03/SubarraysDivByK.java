e6e03bb43a7515ffdb9cc3193a6bf1d2aa21173f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:24:28 +0530

    Update SubarraysDivByK.java

--- Day03/SubarraysDivByK.java
+++ Day03/SubarraysDivByK.java
@@ -10,6 +10,12 @@ class Solution {
         int prefix_sum = 0;
         int count = 0;
 
+        for (int i = 0; i < nums.length; i++) {
+            prefix_sum += nums[i];
+
+            int rem = prefix_sum % k;
+        }
+
         return count;
     }
 }
