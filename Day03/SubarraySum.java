9f41934cf17b0e9ea8215d1083f3d044faa0d0b4
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:20:22 +0530

    Update SubarraySum.java

--- Day03/SubarraySum.java
+++ Day03/SubarraySum.java
@@ -8,6 +8,12 @@ class Solution {
 
         hm.put(0, 1);
 
-        return 0;
+        int noOfSubArrayPossible = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+        }
+
+        return noOfSubArrayPossible;
     }
 }
