ffa46d17ddecc19fa077570bc4b0885722802835
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:19:45 +0530

    Update SubarraySum.java

--- Day03/SubarraySum.java
+++ Day03/SubarraySum.java
@@ -4,6 +4,8 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> hm = new HashMap<>();
 
+        int prefixSum = 0;
+
         return 0;
     }
 }
