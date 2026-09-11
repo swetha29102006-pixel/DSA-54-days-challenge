3c8844e32fb355e4d20083e290a015b56b7c7899
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:34:59 +0530

    Update ShortestSubarray.java

--- Day03/ShortestSubarray.java
+++ Day03/ShortestSubarray.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 class Solution {
     public int shortestSubarray(int[] nums, int k) {
+        int res = Integer.MAX_VALUE;
+        long curSum = 0;
+
         return -1;
     }
 }
