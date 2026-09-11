6ce5e176b9d749786695ac00a059dc7f905898af
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:21:42 +0530

    Complete Subarray Sum solution

--- Day03/SubarraySum.java
+++ Day03/SubarraySum.java
@@ -2,10 +2,12 @@ import java.util.HashMap;
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        // HashMap stores prefix sum and its frequency
         HashMap<Integer, Integer> hm = new HashMap<>();
 
         int prefixSum = 0;
 
+        // Handles subarrays starting from index 0
         hm.put(0, 1);
 
         int noOfSubArrayPossible = 0;
@@ -13,12 +15,15 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             prefixSum += nums[i];
 
+            // Find the prefix sum that needs to be removed
             int remove = prefixSum - k;
 
+            // Add the number of matching prefix sums
             if (hm.containsKey(remove)) {
                 noOfSubArrayPossible += hm.get(remove);
             }
 
+            // Store current prefix sum frequency
             hm.put(prefixSum, hm.getOrDefault(prefixSum, 0) + 1);
         }
 
