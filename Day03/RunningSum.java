9075a1a3aa0a5eb629b1c8775a5497808b6ef784
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:15:00 +0530

    Update RunningSum.java

--- Day03/RunningSum.java
+++ Day03/RunningSum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
-            nums[i - 1];
+            int previous = nums[i - 1];
         }
 
         return nums;
