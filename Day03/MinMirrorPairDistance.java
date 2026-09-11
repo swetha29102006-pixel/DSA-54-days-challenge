03bc3819b8de0d069aa318328d70256da0207022
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:40:05 +0530

    Update MinMirrorPairDistance.java

--- Day03/MinMirrorPairDistance.java
+++ Day03/MinMirrorPairDistance.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
+        int ans = Integer.MAX_VALUE;
 
         return -1;
     }
