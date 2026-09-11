31509b71b40fe0b5b3d38a9c5b19252dd54532b8
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:23:47 +0530

    Update SubarraysDivByK.java

--- Day03/SubarraysDivByK.java
+++ Day03/SubarraysDivByK.java
@@ -4,6 +4,8 @@ class Solution {
     public int subarraysDivByK(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        map.put(0, 1);
+
         return 0;
     }
 }
