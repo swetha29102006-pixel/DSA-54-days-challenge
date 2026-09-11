dabecc0005fb5fee8cd37bbf0eb889da58d6fdc4
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:41:38 +0530

    Update MinMirrorPairDistance.java

--- Day03/MinMirrorPairDistance.java
+++ Day03/MinMirrorPairDistance.java
@@ -9,9 +9,21 @@ class Solution {
         map.put(nums[n - 1], n - 1);
 
         for (int i = n - 2; i >= 0; i--) {
-
+            int rev = reverse(nums[i]);
         }
 
         return -1;
+    }
+
+    public int reverse(int n) {
+        int t = 0;
+
+        while (n > 0) {
+            t *= 10;
+            t += n % 10;
+            n /= 10;
+        }
+
+        return t;
     }
 }
