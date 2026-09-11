596f0c24dc79468b1df9e4733a81c445085cd00c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:10:18 +0530

    Update PivotIndex.java

--- Day03/PivotIndex.java
+++ Day03/PivotIndex.java
@@ -10,6 +10,12 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int rightTotal = total - leftTotal - nums[i];
+
+            if (rightTotal == leftTotal) {
+                return i;
+            }
+
+            leftTotal += nums[i];
         }
     }
 }
