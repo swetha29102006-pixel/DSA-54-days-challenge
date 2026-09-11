dc57ddabd9c11cbb19f40b8232ee1737fe8cd5c8
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-11 20:35:25 +0530

    Update ShortestSubarray.java

--- Day03/ShortestSubarray.java
+++ Day03/ShortestSubarray.java
@@ -5,6 +5,26 @@ class Solution {
         int res = Integer.MAX_VALUE;
         long curSum = 0;
 
+        Deque<Pair<Long, Integer>> q = new ArrayDeque<>();
+
         return -1;
+    }
+
+    static class Pair<K, V> {
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
     }
 }
