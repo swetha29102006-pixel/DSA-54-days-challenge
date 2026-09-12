263370e04538a78cab6e02ab55b8adbfe92f2612
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-12 20:14:25 +0530

    Update PalindromePartitioning.java

--- Day04/PalindromePartitioning.java
+++ Day04/PalindromePartitioning.java
@@ -1,6 +1,13 @@
 class Solution {
 
     private boolean isPalindrome(String s, int start, int end) {
+
+        while (start <= end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
