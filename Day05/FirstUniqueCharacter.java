595fc10a22b56ee899c62378d90c8f8bb0d59579
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-14 19:43:04 +0530

    Update FirstUniqueCharacter.java

--- Day05/FirstUniqueCharacter.java
+++ Day05/FirstUniqueCharacter.java
@@ -9,7 +9,7 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
 
-            if (freq[s.charAt(i) - 'a'] ==} 1) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
