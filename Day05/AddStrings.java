fccdf69d1c5e28cea505042d61844b7681e1b450
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-14 19:49:50 +0530

    Update AddStrings.java

--- Day05/AddStrings.java
+++ Day05/AddStrings.java
@@ -1,5 +1,10 @@
 class Solution {
     public String addStrings(String num1, String num2) {
 
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        int carry = 0;
+
     }
 }
