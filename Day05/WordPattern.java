cdd7db39f0614cd5f8df22f1ff8487f543c934a9
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-14 19:37:07 +0530

    Update WordPattern.java

--- Day05/WordPattern.java
+++ Day05/WordPattern.java
@@ -10,5 +10,12 @@ class Solution {
         HashMap<Character, String> map1 = new HashMap<>();
         HashMap<String, Character> map2 = new HashMap<>();
 
+        for (int i = 0; i < pattern.length(); i++) {
+
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+        }
+
     }
 }
