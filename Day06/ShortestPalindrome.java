11f87d81b786d3c2e41d63dc1ca5dfce5a214bb2
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 18:57:55 +0530

    Update ShortestPalindrome.java

--- Day06/ShortestPalindrome.java
+++ Day06/ShortestPalindrome.java
@@ -3,5 +3,9 @@ class Solution {
 
         String rev = new StringBuilder(s).reverse().toString();
 
+        String combined = s + "#" + rev;
+
+        int[] lps = new int[combined.length()];
+
     }
 }
