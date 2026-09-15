a475e8114a3af03b2b19baaa502e09a36da091ac
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:37:44 +0530

    Update WordBreak.java

--- Day06/WordBreak.java
+++ Day06/WordBreak.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreak(s, wordDict, 0);
+        Boolean[] memo = new Boolean[s.length() + 1];
+        return wordBreak(s, wordDict, 0, memo);
     }
 
-    boolean wordBreak(String s, List<String> wordDict, int k) {
+    boolean wordBreak(String s, List<String> wordDict, int k, Boolean[] memo) {
         if (k == s.length()) {
             return true;
+        }
+
+        if (memo[k] != null) {
+            return memo[k];
         }
 
         for (int i = 0; i < wordDict.size(); i++) {
             String word = wordDict.get(i);
 
             if (s.startsWith(word, k)) {
-                if (wordBreak(s, wordDict, k + word.length())) {
-                    return true;
+                if (wordBreak(s, wordDict, k + word.length(), memo)) {
+                    return memo[k] = true;
                 }
             }
         }
 
-        return false;
+        return memo[k] = false;
     }
 }
