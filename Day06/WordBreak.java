3129ec6477047c9b3e38e0109508c0d24c33861a
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:36:56 +0530

    Update WordBreak.java

--- Day06/WordBreak.java
+++ Day06/WordBreak.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreak(s, wordDict, 0);
+    }
+
+    boolean wordBreak(String s, List<String> wordDict, int k) {
+        if (k == s.length()) {
+            return true;
+        }
+
         return false;
     }
 }
