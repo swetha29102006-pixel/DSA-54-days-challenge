c328f9093a6c6afecf5634f7194f701efdaea0a0
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:53:47 +0530

    Update IntersectionOfTwoLinkedLists.java

--- Day07/IntersectionOfTwoLinkedLists.java
+++ Day07/IntersectionOfTwoLinkedLists.java
@@ -4,18 +4,8 @@ public class Solution {
         ListNode b = headB;
 
         while (a != b) {
-
-            if (a == null) {
-                a = headB;
-            } else {
-                a = a.next;
-            }
-
-            if (b == null) {
-                b = headA;
-            } else {
-                b = b.next;
-            }
+            a = a != null ? a.next : headB;
+            b = b != null ? b.next : headA;
         }
 
         return a;
