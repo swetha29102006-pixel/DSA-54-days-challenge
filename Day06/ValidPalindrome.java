3cc0b17602d2b0b2392cbdae85f6748165f694da
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:05:09 +0530

    Update ValidPalindrome.java

--- Day06/ValidPalindrome.java
+++ Day06/ValidPalindrome.java
@@ -7,7 +7,6 @@ class Solution {
         while (left < right) {
 
             if (s.charAt(left) != s.charAt(right)) {
-
                 return isPalindrome(s, left + 1, right)
                     || isPalindrome(s, left, right - 1);
             }
