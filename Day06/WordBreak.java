aa8c0dd619c2821a823895f61d2bfa7a85da7e62
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:38:40 +0530

    Update WordBreak.java

--- Day06/WordBreak.java
+++ Day06/WordBreak.java
@@ -3,6 +3,35 @@ class Solution {
         return recWay1(s, wordDict);
     }
 
+    boolean recWay2(String s, List<String> wordDict) {
+        Boolean[] memo = new Boolean[s.length() + 1];
+        return wordBreak2(s, new HashSet<>(wordDict), 0, memo);
+    }
+
+    boolean wordBreak2(String s, Set<String> wordDict, int k, Boolean[] memo) {
+        int n = s.length();
+
+        if (k == n) {
+            return true;
+        }
+
+        if (memo[k] != null) {
+            return memo[k];
+        }
+
+        for (int i = k + 1; i <= n; i++) {
+            String word = s.substring(k, i);
+
+            if (wordDict.contains(word)) {
+                if (wordBreak2(s, wordDict, i, memo)) {
+                    return memo[k] = true;
+                }
+            }
+        }
+
+        return memo[k] = false;
+    }
+
     boolean recWay1(String s, List<String> wordDict) {
         Boolean[] memo = new Boolean[s.length() + 1];
         return wordBreak(s, wordDict, 0, memo);
