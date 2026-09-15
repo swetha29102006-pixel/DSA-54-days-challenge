6d56630480f6f101a7ba6578a4967c34709c1dc6
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:21:15 +0530

    Update PalindromePairs.java

--- Day06/PalindromePairs.java
+++ Day06/PalindromePairs.java
@@ -4,5 +4,8 @@ class Solution {
         Map<String, Integer> wmap = new HashMap<>();
         List<List<Integer>> ans = new ArrayList<>();
 
+        for (int i = 0; i < words.length; i++)
+            wmap.put(words[i], i);
+
     }
 }
