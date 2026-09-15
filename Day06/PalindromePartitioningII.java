1b4e70aaf854745642474d12f42f35a618c5cb80
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:30:39 +0530

    Update PalindromePartitioningII.java

--- Day06/PalindromePartitioningII.java
+++ Day06/PalindromePartitioningII.java
@@ -9,6 +9,11 @@ class Solution {
         for (int end = 0; end < n; end++) {
             for (int start = 0; start <= end; start++) {
 
+                if (s.charAt(start) == s.charAt(end) &&
+                    (end - start <= 2 || isPalindrome[start + 1][end - 1])) {
+
+                    isPalindrome[start][end] = true;
+                }
             }
         }
 
