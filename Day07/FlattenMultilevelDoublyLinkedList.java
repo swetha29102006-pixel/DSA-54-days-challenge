cd61b0ff093cd96dcb53f6837be3361311abbfc9
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 20:02:23 +0530

    Update FlattenMultilevelDoublyLinkedList.java

--- Day07/FlattenMultilevelDoublyLinkedList.java
+++ Day07/FlattenMultilevelDoublyLinkedList.java
@@ -2,6 +2,12 @@ class Solution {
     public Node flatten(Node head) {
         if (head == null) return null;
 
+        Node curr = head;
+
+        while (curr != null) {
+            curr = curr.next;
+        }
+
         return head;
     }
 }
