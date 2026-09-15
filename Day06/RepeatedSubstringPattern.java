1d530d06c321489cc93128e587ba0734860d8e5c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 18:52:30 +0530

    Update RepeatedSubstringPattern.java

--- Day06/RepeatedSubstringPattern.java
+++ Day06/RepeatedSubstringPattern.java
@@ -12,6 +12,17 @@ class Solution {
             String part = s.substring(0, len);
             boolean valid = true;
 
+            for (int i = len; i < n; i += len) {
+
+                if (!s.substring(i, i + len).equals(part)) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                return true;
+            }
         }
 
         return false;
