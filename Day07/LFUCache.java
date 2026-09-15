bb530fe20cbb0c08ded749477ceb10f073d7e7d7
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 20:07:00 +0530

    Update LFUCache.java

--- Day07/LFUCache.java
+++ Day07/LFUCache.java
@@ -1,7 +1,10 @@
 class LFUCache {
 
     private int capacity;
+    private int minFreq = 0;
+
     private Map<Integer, Integer> keyToVal = new HashMap<>();
+    private Map<Integer, Integer> keyToFreq = new HashMap<>();
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
@@ -10,6 +13,8 @@ class LFUCache {
     public int get(int key) {
         if (!keyToVal.containsKey(key))
             return -1;
+
+        final int freq = keyToFreq.get(key);
 
         return keyToVal.get(key);
     }
