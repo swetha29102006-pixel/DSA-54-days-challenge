bf969a5696064f252b21323fac4896e0356007c5
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 19:25:10 +0530

    Update PalindromePartitioningII.java

--- Day06/PalindromePartitioningII.java
+++ Day06/PalindromePartitioningII.java
@@ -1,5 +1,10 @@
 class Solution {
     public int minCut(String s) {
 
+        int n = s.length();
+
+        boolean[][] isPalindrome = new boolean[n][n];
+        int[] minCuts = new int[n];
+
     }
 }
