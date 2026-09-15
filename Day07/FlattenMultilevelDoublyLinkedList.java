319461942a51ca5f460f52f3bc73992b75a3cc36
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-15 20:04:42 +0530

    Update FlattenMultilevelDoublyLinkedList.java

--- Day07/FlattenMultilevelDoublyLinkedList.java
+++ Day07/FlattenMultilevelDoublyLinkedList.java
@@ -6,20 +6,26 @@ class Solution {
 
         while (curr != null) {
             if (curr.child != null) {
+
                 Node nextNode = curr.next;
                 Node childTail = curr.child;
 
+                // Find the end of child list
                 while (childTail.next != null) {
                     childTail = childTail.next;
                 }
 
+                // Connect child list to next part
                 if (nextNode != null) {
                     childTail.next = nextNode;
                     nextNode.prev = childTail;
                 }
 
+                // Connect current node to child list
                 curr.next = curr.child;
                 curr.child.prev = curr;
+
+                // Remove child pointer
                 curr.child = null;
             }
 
