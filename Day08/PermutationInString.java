f14092f1b7650bece65b4c9ce5b4fd0bdb9c7887
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-16 19:16:57 +0530

    Update PermutationInString.java

--- Day08/PermutationInString.java
+++ Day08/PermutationInString.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
+        int p[] = new int[26];
+        int w[] = new int[26];
+        int l = 0;
+        int k = s1.length();
     }
 }
