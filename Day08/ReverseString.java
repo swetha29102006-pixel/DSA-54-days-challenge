b6804392f7213511ad36ee2dd1aa7b53d1992578
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-16 19:42:54 +0530

    Update ReverseString.java

--- Day08/ReverseString.java
+++ Day08/ReverseString.java
@@ -2,5 +2,10 @@ class Solution {
     public void reverseString(char[] s) {
         int left = 0;
         int right = s.length - 1;
+
+        while (left < right) {
+            left++;
+            right--;
+        }
     }
 }
