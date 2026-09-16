96f0e18192a00b28f287d80d6259c2f6d8e567ed
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-16 19:33:08 +0530

    Update SubarraysWithKDifferentIntegers.java

--- Day08/SubarraysWithKDifferentIntegers.java
+++ Day08/SubarraysWithKDifferentIntegers.java
@@ -1,5 +1,9 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
+        return check(nums, k);
+    }
+
+    public int check(int[] nums, int k) {
         return 0;
     }
 }
