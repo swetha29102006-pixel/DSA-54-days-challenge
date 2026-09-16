f956b8d86f9b564cf1e948af18f6275565a4e2f0
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-16 19:31:03 +0530

    Update LongestRepeatingCharacterReplacement.java

--- Day08/LongestRepeatingCharacterReplacement.java
+++ Day08/LongestRepeatingCharacterReplacement.java
@@ -1,6 +1,7 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-
+        int l = 0;
+        int r = 0;
         HashMap<Character, Integer> m = new HashMap<>();
         int maxFreq = 0;
         int maxLen = 0;
