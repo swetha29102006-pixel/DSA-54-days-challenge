55b782bc5b0b2db45463d06f59e4a1d8bcd3a98c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-16 19:13:33 +0530

    Update MinimumSizeSubarraySum.java

--- Day08/MinimumSizeSubarraySum.java
+++ Day08/MinimumSizeSubarraySum.java
@@ -1,22 +1,26 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+
         int minLen = Integer.MAX_VALUE;
         int left = 0;
         int curSum = 0;
 
         for (int right = 0; right < nums.length; right++) {
+
+            // Expand window
             curSum += nums[right];
 
+            // Shrink window while sum is enough
             while (curSum >= target) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                }
+
+                minLen = Math.min(minLen, right - left + 1);
 
                 curSum -= nums[left];
                 left++;
             }
         }
 
+        // Return 0 if no valid subarray exists
         return minLen != Integer.MAX_VALUE ? minLen : 0;
     }
 }
