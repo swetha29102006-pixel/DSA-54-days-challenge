a0fd55db8c90b11e8a98a8712b0cb8dcab8a7764
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-16 19:33:45 +0530

    Update SubarraysWithKDifferentIntegers.java

--- Day08/SubarraysWithKDifferentIntegers.java
+++ Day08/SubarraysWithKDifferentIntegers.java
@@ -4,6 +4,11 @@ class Solution {
     }
 
     public int check(int[] nums, int k) {
-        return 0;
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+        HashMap<Integer, Integer> h = new HashMap<>();
+
+        return cnt;
     }
 }
