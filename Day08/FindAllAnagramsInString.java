f4cfb18d44ee15cc2d86daa1f0c89b6679243a39
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-16 19:22:52 +0530

    Update FindAllAnagramsInString.java

--- Day08/FindAllAnagramsInString.java
+++ Day08/FindAllAnagramsInString.java
@@ -1,5 +1,9 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
+
+        HashMap<Character, Integer> originalMap = new HashMap<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        return arr;
     }
 }
