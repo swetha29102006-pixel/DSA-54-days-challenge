7062af360fb677e429d7b88ea7cefb219b0d5e15
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:52 +0530

    Update AsteroidCollision.java

--- Day09/AsteroidCollision.java
+++ Day09/AsteroidCollision.java
@@ -1,5 +1,8 @@
+import java.util.Stack;
+
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
         return new int[0];
     }
 }
