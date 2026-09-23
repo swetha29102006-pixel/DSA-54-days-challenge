885f32949a9a506103ab8e471300fa5c99a3d67b
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:42 +0530

    Update MinStack.java

--- Day09/MinStack.java
+++ Day09/MinStack.java
@@ -1,6 +1,12 @@
+import java.util.Stack;
+
 class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
     public MinStack() {
-        
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
     
     public void push(int val) {
