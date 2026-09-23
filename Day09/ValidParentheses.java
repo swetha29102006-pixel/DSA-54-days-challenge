03e1cd62eb531126981ac611be1b281ecde53f33
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:40 +0530

    Update ValidParentheses.java

--- Day09/ValidParentheses.java
+++ Day09/ValidParentheses.java
@@ -3,6 +3,11 @@ import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            
+        }
+
         return false;
     }
 }
