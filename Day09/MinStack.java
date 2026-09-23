e3599449eef47fa20beaac9e63bf1b7faae8907f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:41 +0530

    Create MinStack.java

---
+++ Day09/MinStack.java
@@ -0,0 +1,21 @@
+class MinStack {
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        return 0;
+    }
+    
+    public int getMin() {
+        return 0;
+    }
+}
