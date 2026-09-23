851279d386dceacb464b31e8e877278e94b818f8
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:29 +0530

    Update KokoEatingBananas.java

--- Day10/KokoEatingBananas.java
+++ Day10/KokoEatingBananas.java
@@ -1,5 +1,11 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        return 0;
+        int left = 1;
+        int right = 0;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        return right;
     }
 }
