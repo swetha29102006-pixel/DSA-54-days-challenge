a4c6472140cad203b0d76dd3d59bf3e03fadb292
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:25 +0530

    Update SingleElementInSortedArray.java

--- Day10/SingleElementInSortedArray.java
+++ Day10/SingleElementInSortedArray.java
@@ -1,5 +1,8 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        return 0;
+        int left = 0;
+        int right = nums.length - 1;
+
+        return nums[0];
     }
 }
