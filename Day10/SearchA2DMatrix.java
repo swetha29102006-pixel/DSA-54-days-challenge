ae353ca98359427f941d2106506c4e0780290087
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:27 +0530

    Update SearchA2DMatrix.java

--- Day10/SearchA2DMatrix.java
+++ Day10/SearchA2DMatrix.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = m * n - 1;
+
         return false;
     }
 }
