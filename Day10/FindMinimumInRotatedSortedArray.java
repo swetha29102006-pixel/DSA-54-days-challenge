26dfeca869f3ce7f9f35e6d2c834f58ef85086bd
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:23 +0530

    Update FindMinimumInRotatedSortedArray.java

--- Day10/FindMinimumInRotatedSortedArray.java
+++ Day10/FindMinimumInRotatedSortedArray.java
@@ -4,7 +4,7 @@ class Solution {
         int right = nums.length - 1;
 
         while (left < right) {
-            
+            int mid = left + (right - left) / 2;
         }
 
         return nums[left];
