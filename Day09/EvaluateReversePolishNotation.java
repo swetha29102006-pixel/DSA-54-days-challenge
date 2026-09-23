7d862e229a086e76d4d2c5dc9b0747216eddc0fc
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:45 +0530

    Update EvaluateReversePolishNotation.java

--- Day09/EvaluateReversePolishNotation.java
+++ Day09/EvaluateReversePolishNotation.java
@@ -5,9 +5,18 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (String token : tokens) {
-            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+            if (token.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
                 int b = stack.pop();
                 int a = stack.pop();
+                stack.push(a - b);
+            } else if (token.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
             } else {
                 stack.push(Integer.parseInt(token));
             }
