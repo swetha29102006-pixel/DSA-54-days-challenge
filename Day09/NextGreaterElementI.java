fc3a6d0e79145a1958585be51170899b90554ce0
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:50 +0530

    Update NextGreaterElementI.java

--- Day09/NextGreaterElementI.java
+++ Day09/NextGreaterElementI.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
         return new int[0];
     }
 }
