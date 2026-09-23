8825f867e2c22055fafee10e103e873cbde2b70f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:25 +0530

    Update SingleElementInSortedArray.java

--- Day10/SingleElementInSortedArray.java
+++ Day10/SingleElementInSortedArray.java
@@ -4,7 +4,10 @@ class Solution {
         int right = nums.length - 1;
 
         while (left < right) {
-            
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
         }
 
         return nums[left];
