747399051706bb3bcebb0c084bdf2c738ec59a29
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:47 +0530

    Update DailyTemperatures.java

--- Day09/DailyTemperatures.java
+++ Day09/DailyTemperatures.java
@@ -7,7 +7,9 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < n; i++) {
-            
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+            }
         }
 
         return result;
