ca3981fb88ff9610284493d112e006b3c6b375ed
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:53 +0530

    Update AsteroidCollision.java

--- Day09/AsteroidCollision.java
+++ Day09/AsteroidCollision.java
@@ -22,6 +22,11 @@ class Solution {
             }
         }
 
-        return new int[0];
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
     }
 }
