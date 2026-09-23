17d0e2eef61c55445cda81bb06a9af95197a8236
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:48 +0530

    Update DailyTemperatures.java

--- Day09/DailyTemperatures.java
+++ Day09/DailyTemperatures.java
@@ -2,6 +2,10 @@ import java.util.Stack;
 
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        if (temperatures == null || temperatures.length == 0) {
+            return new int[0];
+        }
+
         int n = temperatures.length;
         int[] result = new int[n];
         Stack<Integer> stack = new Stack<>();
