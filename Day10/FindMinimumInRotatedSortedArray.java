cafa22509cf823f8fb7abd272d941c9a52ce9ed2
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:24 +0530

    Update FindMinimumInRotatedSortedArray.java

--- Day10/FindMinimumInRotatedSortedArray.java
+++ Day10/FindMinimumInRotatedSortedArray.java
@@ -7,12 +7,15 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
+        // Binary search for the inflection point in O(log n)
         while (left < right) {
             int mid = left + (right - left) / 2;
 
+            // Minimum must be in the right unsorted part
             if (nums[mid] > nums[right]) {
                 left = mid + 1;
             } else {
+                // Minimum is at mid or in the left part
                 right = mid;
             }
         }
