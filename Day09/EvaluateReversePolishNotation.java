b0010d8c8514ddc23c2557d4bc5498cc40a22d0a
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:44 +0530

    Update EvaluateReversePolishNotation.java

--- Day09/EvaluateReversePolishNotation.java
+++ Day09/EvaluateReversePolishNotation.java
@@ -3,6 +3,11 @@ import java.util.Stack;
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            
+        }
+
         return 0;
     }
 }
