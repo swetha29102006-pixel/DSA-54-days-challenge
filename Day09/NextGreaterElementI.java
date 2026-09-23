b63d2de09b812d39ca8e7cf384a49210e23a7913
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:51 +0530

    Update NextGreaterElementI.java

--- Day09/NextGreaterElementI.java
+++ Day09/NextGreaterElementI.java
@@ -8,7 +8,9 @@ class Solution {
             return new int[0];
         }
 
+        // Map to store next greater element for each number in nums2
         Map<Integer, Integer> map = new HashMap<>();
+        // Monotonic stack to find next greater elements in nums2
         Stack<Integer> stack = new Stack<>();
 
         for (int num : nums2) {
@@ -18,6 +20,7 @@ class Solution {
             stack.push(num);
         }
 
+        // Build result for nums1 using the map
         int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             result[i] = map.getOrDefault(nums1[i], -1);
