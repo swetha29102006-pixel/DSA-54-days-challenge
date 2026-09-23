761e7caa519e763a1086a80ac7422c8f053e5006
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:41 +0530

    Update ValidParentheses.java

--- Day09/ValidParentheses.java
+++ Day09/ValidParentheses.java
@@ -2,12 +2,19 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
+        // Fast exit for odd length
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
+            // Push opening brackets
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
+                // If closing bracket without matching opening bracket
                 if (stack.isEmpty()) {
                     return false;
                 }
