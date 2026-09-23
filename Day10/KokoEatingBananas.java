bb86b73d357b929e34660d602585ef2e90e348da
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:31 +0530

    Update KokoEatingBananas.java

--- Day10/KokoEatingBananas.java
+++ Day10/KokoEatingBananas.java
@@ -10,14 +10,15 @@ class Solution {
             right = Math.max(right, pile);
         }
 
+        // Binary search on answer speed range [1, max(piles)]
         while (left < right) {
             int mid = left + (right - left) / 2;
             long hoursNeeded = getHours(piles, mid);
 
             if (hoursNeeded <= h) {
-                right = mid;
+                right = mid; // Can finish in time; try slower speed
             } else {
-                left = mid + 1;
+                left = mid + 1; // Cannot finish in time; must eat faster
             }
         }
 
@@ -27,6 +28,7 @@ class Solution {
     private long getHours(int[] piles, int speed) {
         long hours = 0;
         for (int pile : piles) {
+            // Integer ceiling division: ceil(pile / speed)
             hours += (pile + speed - 1) / speed;
         }
         return hours;
