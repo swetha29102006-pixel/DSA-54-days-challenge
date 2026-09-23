fb9f46536ac96dd068548e87600c7e74699aafbe
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:27 +0530

    Update SearchA2DMatrix.java

--- Day10/SearchA2DMatrix.java
+++ Day10/SearchA2DMatrix.java
@@ -6,7 +6,8 @@ class Solution {
         int right = m * n - 1;
 
         while (left <= right) {
-            
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / n][mid % n];
         }
 
         return false;
