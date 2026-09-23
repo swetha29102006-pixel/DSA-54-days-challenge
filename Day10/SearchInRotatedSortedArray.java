cebedf5ca0d80130da35209b702b55e4f1a4093d
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:21 +0530

    Update SearchInRotatedSortedArray.java

--- Day10/SearchInRotatedSortedArray.java
+++ Day10/SearchInRotatedSortedArray.java
@@ -1,5 +1,9 @@
 class Solution {
     public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
         int left = 0;
         int right = nums.length - 1;
 
