a7905af84ba39fb5df371bc66fc6191c6c724614
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:26 +0530

    Update SingleElementInSortedArray.java

--- Day10/SingleElementInSortedArray.java
+++ Day10/SingleElementInSortedArray.java
@@ -7,12 +7,15 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
+        // Binary search based on even/odd index parity in O(log n)
         while (left < right) {
             int mid = left + (right - left) / 2;
+            // Ensure mid is even
             if (mid % 2 == 1) {
                 mid--;
             }
 
+            // If pair starts at even index, unique element is to the right
             if (nums[mid] == nums[mid + 1]) {
                 left = mid + 2;
             } else {
