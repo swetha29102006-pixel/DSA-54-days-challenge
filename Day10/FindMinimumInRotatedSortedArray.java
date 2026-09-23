07791376f6d1526b05922734035076e0d4f5e0c6
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:24 +0530

    Update FindMinimumInRotatedSortedArray.java

--- Day10/FindMinimumInRotatedSortedArray.java
+++ Day10/FindMinimumInRotatedSortedArray.java
@@ -1,5 +1,9 @@
 class Solution {
     public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
         int left = 0;
         int right = nums.length - 1;
 
