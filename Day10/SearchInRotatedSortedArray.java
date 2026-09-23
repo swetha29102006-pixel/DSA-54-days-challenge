156275f52bfbe5ec09db8b86c62c15f1d4bc3d42
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:10:20 +0530

    Update SearchInRotatedSortedArray.java

--- Day10/SearchInRotatedSortedArray.java
+++ Day10/SearchInRotatedSortedArray.java
@@ -4,7 +4,11 @@ class Solution {
         int right = nums.length - 1;
 
         while (left <= right) {
-            
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
         }
 
         return -1;
