a7c260caa42ff9252621636295563156eb22c9a9
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-23 09:01:46 +0530

    Update DailyTemperatures.java

--- Day09/DailyTemperatures.java
+++ Day09/DailyTemperatures.java
@@ -1,5 +1,11 @@
+import java.util.Stack;
+
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        return new int[0];
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        return result;
     }
 }
