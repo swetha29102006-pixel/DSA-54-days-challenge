5aeb862af9e43dd994fa9975ca97e529f20c7aea
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:09 +0530

    Update QuickSort.java

--- Day12/QuickSort.java
+++ Day12/QuickSort.java
@@ -10,12 +10,23 @@ class Solution {
     private void sort(int[] nums, int low, int high) {
         if (low < high) {
             int pIndex = partition(nums, low, high);
+            sort(nums, low, pIndex - 1);
+            sort(nums, pIndex + 1, high);
         }
     }
 
     private int partition(int[] nums, int low, int high) {
         int pivot = nums[high];
         int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (nums[j] <= pivot) {
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
         return high;
     }
 }
