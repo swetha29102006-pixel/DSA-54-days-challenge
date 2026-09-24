cbe97c610b95b337f5720a438e7df29c73601b03
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:05 +0530

    Update InsertionSort.java

--- Day12/InsertionSort.java
+++ Day12/InsertionSort.java
@@ -6,10 +6,12 @@ class Solution {
 
         int n = nums.length;
 
+        // Build sorted prefix one element at a time
         for (int i = 1; i < n; i++) {
             int key = nums[i];
             int j = i - 1;
 
+            // Move elements greater than key one position ahead
             while (j >= 0 && nums[j] > key) {
                 nums[j + 1] = nums[j];
                 j--;
