fe0de3c143ac93f7ff9d5c04172cd0a815ee9c8f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:06 +0530

    Update MergeSort.java

--- Day12/MergeSort.java
+++ Day12/MergeSort.java
@@ -8,6 +8,11 @@ class Solution {
     }
 
     private void sort(int[] nums, int left, int right) {
-        
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        sort(nums, left, mid);
+        sort(nums, mid + 1, right);
     }
 }
