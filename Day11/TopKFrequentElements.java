e6f0ae146841978f77be33bd236ede7e5f935e97
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:28 +0530

    Update TopKFrequentElements.java

--- Day11/TopKFrequentElements.java
+++ Day11/TopKFrequentElements.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
         return new int[0];
     }
 }
