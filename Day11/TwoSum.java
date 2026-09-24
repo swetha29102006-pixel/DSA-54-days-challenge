f04a99a5b1861bf319a9556f63eb1368227f5b5d
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:18 +0530

    Update TwoSum.java

--- Day11/TwoSum.java
+++ Day11/TwoSum.java
@@ -7,16 +7,16 @@ class Solution {
             return new int[0];
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> numToIndex = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
 
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+            if (numToIndex.containsKey(complement)) {
+                return new int[]{numToIndex.get(complement), i};
             }
 
-            map.put(nums[i], i);
+            numToIndex.put(nums[i], i);
         }
 
         return new int[0];
