046eb60da2df8b8eefff2d40df1d91b29ff9f342
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:16 +0530

    Update TwoSum.java

--- Day11/TwoSum.java
+++ Day11/TwoSum.java
@@ -1,5 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
         return new int[0];
     }
 }
