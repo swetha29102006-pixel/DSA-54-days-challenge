05faa72e283b3c3d04291634e585f53ea8867be2
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:01 +0530

    Update SelectionSort.java

--- Day12/SelectionSort.java
+++ Day12/SelectionSort.java
@@ -1,5 +1,11 @@
 class Solution {
     public int[] selectionSort(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+        }
+
         return nums;
     }
 }
