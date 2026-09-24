d3e019062822a0a60ebc1761a3769ba22349b056
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:25 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- Day13/LongestSubstringWithoutRepeatingCharacters.java
+++ Day13/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        int maxLen = 0;
+        int left = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        return maxLen;
     }
 }
