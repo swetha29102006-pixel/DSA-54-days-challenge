690c340cdc34dc39dc0bb249e08ede12deaadd09
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:30 +0530

    Create InsertDeleteGetRandom.java

---
+++ Day11/InsertDeleteGetRandom.java
@@ -0,0 +1,17 @@
+class RandomizedSet {
+    public RandomizedSet() {
+        
+    }
+    
+    public boolean insert(int val) {
+        return false;
+    }
+    
+    public boolean remove(int val) {
+        return false;
+    }
+    
+    public int getRandom() {
+        return 0;
+    }
+}
