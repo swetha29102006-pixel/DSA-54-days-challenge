34b25404411a6571a15b8fcbd73eaf055f7ab0c8
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:04 +0530

    Update InsertionSort.java

--- Day12/InsertionSort.java
+++ Day12/InsertionSort.java
@@ -1,5 +1,12 @@
 class Solution {
     public int[] insertionSort(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = nums[i];
+            int j = i - 1;
+        }
+
         return nums;
     }
 }
