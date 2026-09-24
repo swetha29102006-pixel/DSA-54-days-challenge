4ab0693e4be8acf7b47916904716bba3f7db4ed4
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:13 +0530

    Update MergeIntervals.java

--- Day12/MergeIntervals.java
+++ Day12/MergeIntervals.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        return new int[0][0];
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        return intervals;
     }
 }
