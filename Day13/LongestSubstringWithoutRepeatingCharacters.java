ce97336a46e6263ca6c3e650947a57f39ae2407e
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:26 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- Day13/LongestSubstringWithoutRepeatingCharacters.java
+++ Day13/LongestSubstringWithoutRepeatingCharacters.java
@@ -3,18 +3,22 @@ import java.util.Map;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
         int maxLen = 0;
         int left = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> lastSeen = new HashMap<>();
 
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
 
-            if (map.containsKey(c)) {
-                left = Math.max(left, map.get(c) + 1);
+            if (lastSeen.containsKey(c)) {
+                left = Math.max(left, lastSeen.get(c) + 1);
             }
 
-            map.put(c, right);
+            lastSeen.put(c, right);
             maxLen = Math.max(maxLen, right - left + 1);
         }
 
