5f9a489e1c140e17129891e7bd3dd0c5abedfdf9
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:03 +0530

    Update SelectionSort.java

--- Day12/SelectionSort.java
+++ Day12/SelectionSort.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[] selectionSort(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+
         int n = nums.length;
 
         for (int i = 0; i < n - 1; i++) {
