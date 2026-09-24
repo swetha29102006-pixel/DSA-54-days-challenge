47bbc3d34c76fd418ce74de12c06754741c69e6b
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:06 +0530

    Update MergeSort.java

--- Day12/MergeSort.java
+++ Day12/MergeSort.java
@@ -1,5 +1,13 @@
 class Solution {
     public int[] mergeSort(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+        sort(nums, 0, nums.length - 1);
         return nums;
+    }
+
+    private void sort(int[] nums, int left, int right) {
+        
     }
 }
