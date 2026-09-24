d78a8a6b863512714beb8f2857fbc63a7fcf532d
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:20 +0530

    Update ThreeSum.java

--- Day13/ThreeSum.java
+++ Day13/ThreeSum.java
@@ -10,6 +10,12 @@ class Solution {
         }
 
         Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+        }
+
         return result;
     }
 }
