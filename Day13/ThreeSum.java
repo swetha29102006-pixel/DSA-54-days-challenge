1b2c68f0bdff7d8b13291074e5331dd027b5567c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:21 +0530

    Update ThreeSum.java

--- Day13/ThreeSum.java
+++ Day13/ThreeSum.java
@@ -21,6 +21,15 @@ class Solution {
 
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
 
