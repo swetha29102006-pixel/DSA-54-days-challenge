c749232af916aed6ec3228ab25214d7fe34006e7
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:12 +0530

    Update SortColors.java

--- Day12/SortColors.java
+++ Day12/SortColors.java
@@ -4,25 +4,28 @@ class Solution {
             return;
         }
 
+        // Dutch National Flag 3-way partition
         int low = 0;
         int mid = 0;
         int high = nums.length - 1;
 
         while (mid <= high) {
             if (nums[mid] == 0) {
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
+                swap(nums, low, mid);
                 low++;
                 mid++;
             } else if (nums[mid] == 1) {
                 mid++;
             } else {
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
+                swap(nums, mid, high);
                 high--;
             }
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
