c91bfe7fbb53085d53f8ad8cf4f934e358563b6a
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:20 +0530

    Update ContainsDuplicate.java

--- Day11/ContainsDuplicate.java
+++ Day11/ContainsDuplicate.java
@@ -3,6 +3,10 @@ import java.util.Set;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+
         Set<Integer> seen = new HashSet<>();
 
         for (int num : nums) {
