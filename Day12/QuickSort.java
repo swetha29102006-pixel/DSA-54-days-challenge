e90180ec348d5212585e39656be675eae962b5bb
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:09 +0530

    Update QuickSort.java

--- Day12/QuickSort.java
+++ Day12/QuickSort.java
@@ -1,5 +1,13 @@
 class Solution {
     public int[] quickSort(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+        sort(nums, 0, nums.length - 1);
         return nums;
+    }
+
+    private void sort(int[] nums, int low, int high) {
+        
     }
 }
