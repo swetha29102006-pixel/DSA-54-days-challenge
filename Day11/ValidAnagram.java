ce3dc71469648bf7227e1b6843563e6f05d824c2
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:21 +0530

    Update ValidAnagram.java

--- Day11/ValidAnagram.java
+++ Day11/ValidAnagram.java
@@ -1,5 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
         return false;
     }
 }
