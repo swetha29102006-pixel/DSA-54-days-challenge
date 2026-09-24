c6276d3a069dc332e6764c7178d9d29e13a658dc
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:19 +0530

    Update ContainsDuplicate.java

--- Day11/ContainsDuplicate.java
+++ Day11/ContainsDuplicate.java
@@ -1,5 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
         return false;
     }
 }
