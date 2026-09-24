ee659550b507898f7d4faa9515b330462c16d06c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:23 +0530

    Update GroupAnagrams.java

--- Day11/GroupAnagrams.java
+++ Day11/GroupAnagrams.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        return new ArrayList<>(map.values());
     }
 }
