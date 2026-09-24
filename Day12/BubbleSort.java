8d7abd7ea8f2ccf8583ee941c6166a7785cdd1d5
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:01 +0530

    Update BubbleSort.java

--- Day12/BubbleSort.java
+++ Day12/BubbleSort.java
@@ -1,4 +1,9 @@
 class Solution {
+    /**
+     * Bubble Sort implementation with early exit optimization.
+     * Time Complexity: O(n^2) worst/average, O(n) best case.
+     * Space Complexity: O(1) auxiliary space.
+     */
     public int[] bubbleSort(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return nums;
@@ -6,9 +11,11 @@ class Solution {
 
         int n = nums.length;
 
-        // Perform bubble sort with early exit optimization
+        // Traverse through all array elements
         for (int i = 0; i < n - 1; i++) {
             boolean swapped = false;
+
+            // Last i elements are already in place
             for (int j = 0; j < n - 1 - i; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
@@ -17,7 +24,8 @@ class Solution {
                     swapped = true;
                 }
             }
-            // If no swaps occurred, array is already sorted
+
+            // Early exit if array is already sorted
             if (!swapped) {
                 break;
             }
