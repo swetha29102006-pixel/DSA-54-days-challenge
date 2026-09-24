8d76355c9b561fb04f66475f744250f4520d5414
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:00 +0530

    Update BubbleSort.java

--- Day12/BubbleSort.java
+++ Day12/BubbleSort.java
@@ -4,7 +4,11 @@ class Solution {
 
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
-                
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
             }
         }
 
