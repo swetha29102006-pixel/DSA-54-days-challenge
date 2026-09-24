beb289f5fc8afaa0cf024ce1958a924f5d7b8c96
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:29 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- Day13/RemoveDuplicatesFromSortedArray.java
+++ Day13/RemoveDuplicatesFromSortedArray.java
@@ -1,12 +1,18 @@
 class Solution {
+    /**
+     * Removes duplicates in-place from sorted array using two pointers.
+     * Time Complexity: O(n)
+     * Space Complexity: O(1) in-place
+     */
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
 
-        // Two-pointer read-write technique
+        // Slow write pointer tracks boundary of unique prefix
         int writeIndex = 0;
 
+        // Fast read pointer scans remaining elements
         for (int readIndex = 1; readIndex < nums.length; readIndex++) {
             if (nums[readIndex] != nums[writeIndex]) {
                 writeIndex++;
@@ -14,6 +20,7 @@ class Solution {
             }
         }
 
+        // Number of unique elements is writeIndex + 1
         return writeIndex + 1;
     }
 }
