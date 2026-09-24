bd459e16f63f4c2d28b42b9846628ea2ec92149f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:21 +0530

    Update ThreeSum.java

--- Day13/ThreeSum.java
+++ Day13/ThreeSum.java
@@ -12,8 +12,16 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
             int left = i + 1;
             int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+            }
         }
 
         return result;
