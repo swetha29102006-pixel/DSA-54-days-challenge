aa4f61dad3a622a0059fc5103a3b012f3441ece5
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:15 +0530

    Update MergeIntervals.java

--- Day12/MergeIntervals.java
+++ Day12/MergeIntervals.java
@@ -8,6 +8,7 @@ class Solution {
             return intervals;
         }
 
+        // Sort intervals based on starting boundary
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> merged = new ArrayList<>();
 
@@ -16,6 +17,7 @@ class Solution {
 
         for (int i = 1; i < intervals.length; i++) {
             int[] next = intervals[i];
+            // If overlapping, extend end time
             if (current[1] >= next[0]) {
                 current[1] = Math.max(current[1], next[1]);
             } else {
