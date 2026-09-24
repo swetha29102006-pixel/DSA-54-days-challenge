5cfc44e749132abcf58021cc2e2ba2f087b4b586
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:17 +0530

    Update TwoSum.java

--- Day11/TwoSum.java
+++ Day11/TwoSum.java
@@ -5,6 +5,10 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+        }
+
         return new int[0];
     }
 }
