d237e564452099af08024080b45298158d1291c3
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:18 +0530

    Update TwoSum.java

--- Day11/TwoSum.java
+++ Day11/TwoSum.java
@@ -7,11 +7,13 @@ class Solution {
             return new int[0];
         }
 
+        // Map each number to its corresponding index for O(1) lookup
         Map<Integer, Integer> numToIndex = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
 
+            // If complement exists in map, solution is found
             if (numToIndex.containsKey(complement)) {
                 return new int[]{numToIndex.get(complement), i};
             }
