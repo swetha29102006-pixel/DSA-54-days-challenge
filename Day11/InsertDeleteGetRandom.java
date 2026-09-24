4457c0e798e4945185f5be6c1faa03448798873c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:30 +0530

    Update InsertDeleteGetRandom.java

--- Day11/InsertDeleteGetRandom.java
+++ Day11/InsertDeleteGetRandom.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 class RandomizedSet {
+    private List<Integer> list;
+    private Map<Integer, Integer> map;
+    private Random rand;
+
     public RandomizedSet() {
-        
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        rand = new Random();
     }
     
     public boolean insert(int val) {
