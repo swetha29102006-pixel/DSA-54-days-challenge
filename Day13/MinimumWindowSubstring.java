eeaf93869e7bd2da4371036e5a32cb1b65f01f2a
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:32 +0530

    Update MinimumWindowSubstring.java

--- Day13/MinimumWindowSubstring.java
+++ Day13/MinimumWindowSubstring.java
@@ -16,6 +16,16 @@ class Solution {
         int formed = 0;
         Map<Character, Integer> windowCounts = new HashMap<>();
 
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+
+            if (targetMap.containsKey(c) && windowCounts.get(c).intValue() == targetMap.get(c).intValue()) {
+                formed++;
+            }
+        }
+
         return "";
     }
 }
