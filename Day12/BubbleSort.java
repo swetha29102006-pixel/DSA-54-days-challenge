fed2692ca0c3fd8dc2e729ab8d311d9ab07ba6f0
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:48:59 +0530

    Update BubbleSort.java

--- Day12/BubbleSort.java
+++ Day12/BubbleSort.java
@@ -1,5 +1,11 @@
 class Solution {
     public int[] bubbleSort(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            
+        }
+
         return nums;
     }
 }
