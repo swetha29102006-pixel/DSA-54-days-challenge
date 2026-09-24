b1dc68c546314e3aad11591d879f8bdacbad6594
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:12 +0530

    Update SortColors.java

--- Day12/SortColors.java
+++ Day12/SortColors.java
@@ -11,6 +11,13 @@ class Solution {
                 nums[mid] = temp;
                 low++;
                 mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
     }
