0c9de296ec730fd0e553ed78e44986c3c1449279
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:27 +0530

    Update LongestConsecutiveSequence.java

--- Day11/LongestConsecutiveSequence.java
+++ Day11/LongestConsecutiveSequence.java
@@ -7,13 +7,16 @@ class Solution {
             return 0;
         }
 
+        // Store all elements in HashSet for O(1) average lookup
         Set<Integer> numSet = new HashSet<>();
         for (int num : nums) {
             numSet.add(num);
         }
 
         int longestStreak = 0;
+
         for (int num : numSet) {
+            // Only start counting streak from the beginning of a sequence
             if (!numSet.contains(num - 1)) {
                 int currentNum = num;
                 int currentStreak = 1;
