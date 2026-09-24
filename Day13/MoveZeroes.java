1599bcda1824d9aa5473957603b7723db8c19f0a
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:31 +0530

    Update MoveZeroes.java

--- Day13/MoveZeroes.java
+++ Day13/MoveZeroes.java
@@ -4,17 +4,18 @@ class Solution {
             return;
         }
 
-        int insertPos = 0;
+        // Two-pointer swap approach to minimize writes
+        int left = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[insertPos] = nums[i];
-                insertPos++;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                if (left != right) {
+                    int temp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = temp;
+                }
+                left++;
             }
-        }
-
-        while (insertPos < nums.length) {
-            nums[insertPos++] = 0;
         }
     }
 }
