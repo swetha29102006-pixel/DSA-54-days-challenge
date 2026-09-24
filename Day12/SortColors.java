dedeb8cef3454d35cc5458ad75a487c4a67d0aea
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:13 +0530

    Update SortColors.java

--- Day12/SortColors.java
+++ Day12/SortColors.java
@@ -1,10 +1,15 @@
 class Solution {
+    /**
+     * Dutch National Flag 3-way partitioning algorithm.
+     * Sorts array containing 0s, 1s, and 2s in-place in a single pass.
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
     public void sortColors(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return;
         }
 
-        // Dutch National Flag 3-way partition
         int low = 0;
         int mid = 0;
         int high = nums.length - 1;
