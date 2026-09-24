25f90c1340253c73d16f38ed950aa39d77a9e029
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:21 +0530

    Update ValidAnagram.java

--- Day11/ValidAnagram.java
+++ Day11/ValidAnagram.java
@@ -8,6 +8,7 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
         return false;
