dbbb9d0e83f040d7516a8207575f55bf407be75e
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:30 +0530

    Update MoveZeroes.java

--- Day13/MoveZeroes.java
+++ Day13/MoveZeroes.java
@@ -3,7 +3,10 @@ class Solution {
         int insertPos = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
         }
     }
 }
