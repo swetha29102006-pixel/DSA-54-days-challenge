1227533d0300c9e50a75c6411298da60851b4f3c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:31 +0530

    Update MoveZeroes.java

--- Day13/MoveZeroes.java
+++ Day13/MoveZeroes.java
@@ -1,5 +1,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+
         int insertPos = 0;
 
         for (int i = 0; i < nums.length; i++) {
