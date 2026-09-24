0ddf76d4c4f2de27001212227f4c40218eae1260
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:32 +0530

    Update MinimumWindowSubstring.java

--- Day13/MinimumWindowSubstring.java
+++ Day13/MinimumWindowSubstring.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() < t.length()) {
+            return "";
+        }
+
+        Map<Character, Integer> targetMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+        }
+
         return "";
     }
 }
