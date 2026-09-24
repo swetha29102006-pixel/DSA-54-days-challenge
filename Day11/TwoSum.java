8e393fd9034808ffd4c2fda76380b12c329d966e
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:17 +0530

    Update TwoSum.java

--- Day11/TwoSum.java
+++ Day11/TwoSum.java
@@ -7,6 +7,10 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
         }
 
         return new int[0];
