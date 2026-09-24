473c605354f3aedbcd828df7c27f9177b29fcced
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:44:28 +0530

    Update TopKFrequentElements.java

--- Day11/TopKFrequentElements.java
+++ Day11/TopKFrequentElements.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class Solution {
@@ -6,6 +8,11 @@ class Solution {
         Map<Integer, Integer> countMap = new HashMap<>();
         for (int num : nums) {
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int i = 0; i <= nums.length; i++) {
+            buckets[i] = new ArrayList<>();
         }
 
         return new int[0];
