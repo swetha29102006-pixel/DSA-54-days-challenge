45f5cf9392c272d89ab118dd7d05bd80fb5d1c0c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:49:03 +0530

    Update SelectionSort.java

--- Day12/SelectionSort.java
+++ Day12/SelectionSort.java
@@ -1,4 +1,9 @@
 class Solution {
+    /**
+     * Selection Sort implementation.
+     * Time Complexity: O(n^2) for all cases.
+     * Space Complexity: O(1) auxiliary space (in-place).
+     */
     public int[] selectionSort(int[] nums) {
         if (nums == null || nums.length <= 1) {
             return nums;
@@ -6,15 +11,18 @@ class Solution {
 
         int n = nums.length;
 
-        // Repeatedly find minimum element from unsorted part
+        // Move the boundary of the unsorted subarray one by one
         for (int i = 0; i < n - 1; i++) {
             int minIdx = i;
+
+            // Find minimum element in unsorted subarray [i..n-1]
             for (int j = i + 1; j < n; j++) {
                 if (nums[j] < nums[minIdx]) {
                     minIdx = j;
                 }
             }
-            // Swap minimum element with first element of unsorted part
+
+            // Swap found minimum element with current element
             if (minIdx != i) {
                 int temp = nums[minIdx];
                 nums[minIdx] = nums[i];
