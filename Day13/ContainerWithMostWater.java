69f1d39246bfffa9b82fe8db254cf163f124aa4f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-24 08:56:17 +0530

    Update ContainerWithMostWater.java

--- Day13/ContainerWithMostWater.java
+++ Day13/ContainerWithMostWater.java
@@ -1,5 +1,9 @@
 class Solution {
     public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+
         int left = 0;
         int right = height.length - 1;
         int max = 0;
