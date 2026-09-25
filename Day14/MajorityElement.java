6abff84398b312e6c58d20d406e775bde95e227b
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:02 +0530

    Update MajorityElement.java

--- Day14/MajorityElement.java
+++ Day14/MajorityElement.java
@@ -4,8 +4,9 @@ class Solution {
             return 0;
         }
 
+        // Boyer-Moore Majority Vote Algorithm
         int count = 0;
-        int candidate = nums[0];
+        int candidate = 0;
 
         for (int num : nums) {
             if (count == 0) {
