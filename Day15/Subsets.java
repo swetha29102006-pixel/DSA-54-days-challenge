a690178d61f2fa644cf5594140433e8328e65345
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:36:03 +0530

    Update Subsets.java

--- Day15/Subsets.java
+++ Day15/Subsets.java
@@ -8,17 +8,19 @@ class Solution {
         }
 
         List<List<Integer>> result = new ArrayList<>();
+        // Recursively build subsets
         backtrack(result, new ArrayList<>(), nums, 0);
         return result;
     }
 
     private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, int start) {
+        // Add copy of current subset to result
         result.add(new ArrayList<>(current));
 
         for (int i = start; i < nums.length; i++) {
-            current.add(nums[i]);
+            current.add(nums[i]); // Include element
             backtrack(result, current, nums, i + 1);
-            current.remove(current.size() - 1);
+            current.remove(current.size() - 1); // Backtrack state
         }
     }
 }
