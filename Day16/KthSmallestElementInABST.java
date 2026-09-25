4a59a3463d99a88baa9cb757023d9c0ec420083c
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:14 +0530

    Create KthSmallestElementInABST.java

---
+++ Day16/KthSmallestElementInABST.java
@@ -0,0 +1,20 @@
+// LeetCode 230: Kth Smallest Element in a BST
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class KthSmallestElementInABST {
+    public int kthSmallest(TreeNode root, int k) {
+        return 0;
+    }
+}
