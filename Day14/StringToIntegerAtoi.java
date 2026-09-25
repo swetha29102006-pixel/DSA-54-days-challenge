44a4f9d3af6a5e761dcd7f52e45f095beee3c74e
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:11 +0530

    Update StringToIntegerAtoi.java

--- Day14/StringToIntegerAtoi.java
+++ Day14/StringToIntegerAtoi.java
@@ -20,6 +20,11 @@ class Solution {
         int result = 0;
         while (index < n && Character.isDigit(s.charAt(index))) {
             int digit = s.charAt(index) - '0';
+
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
             result = result * 10 + digit;
             index++;
         }
