3665ae19c822a50b5564f87adba8723ce34f3fc5
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:36:01 +0530

    Update Subsets.java

--- Day15/Subsets.java
+++ Day15/Subsets.java
@@ -3,6 +3,12 @@ import java.util.List;
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        return new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, int start) {
+
     }
 }
