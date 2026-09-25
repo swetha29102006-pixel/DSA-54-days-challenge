8648c265ba3e1062a7e0a2da568a40eb62b9a08e
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:03 +0530

    Update MaximumSubarray.java

--- Day14/MaximumSubarray.java
+++ Day14/MaximumSubarray.java
@@ -1,5 +1,8 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return 0;
+        int maxSoFar = nums[0];
+        int currentMax = nums[0];
+
+        return maxSoFar;
     }
 }
