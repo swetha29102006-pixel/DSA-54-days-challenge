c96bb2bac07ea93e6ab1a6eb96fc6f799de4e968
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:36:13 +0530

    Update WordSearch.java

--- Day15/WordSearch.java
+++ Day15/WordSearch.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || word == null) {
+            return false;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
         return false;
     }
 }
