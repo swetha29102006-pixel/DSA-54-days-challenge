68aeb412225cf85a164fc4344624fe142209397f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:29 +0530

    Update NQueensII.java

--- Day17/NQueensII.java
+++ Day17/NQueensII.java
@@ -24,6 +24,15 @@ public class NQueensII {
             if (cols[col] || diag1[d1] || diag2[d2]) {
                 continue;
             }
+            cols[col] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+
+            backtrack(row + 1, n, cols, diag1, diag2);
+
+            cols[col] = false;
+            diag1[d1] = false;
+            diag2[d2] = false;
         }
     }
 }
