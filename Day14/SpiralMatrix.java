e868a8124235d14efafb330c3ded4f845a0bf498
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:06 +0530

    Update SpiralMatrix.java

--- Day14/SpiralMatrix.java
+++ Day14/SpiralMatrix.java
@@ -12,7 +12,15 @@ class Solution {
         int left = 0, right = matrix[0].length - 1;
 
         while (top <= bottom && left <= right) {
-            
+            for (int col = left; col <= right; col++) {
+                result.add(matrix[top][col]);
+            }
+            top++;
+
+            for (int row = top; row <= bottom; row++) {
+                result.add(matrix[row][right]);
+            }
+            right--;
         }
 
         return result;
