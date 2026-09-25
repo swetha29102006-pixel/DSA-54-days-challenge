dd2cd9e6d10f7feb570cbf9eff38f42f74b76109
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:08 +0530

    Update LowestCommonAncestor.java

--- Day16/LowestCommonAncestor.java
+++ Day16/LowestCommonAncestor.java
@@ -15,8 +15,8 @@ class TreeNode {
 
 public class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return null;
+        if (root == null || root == p || root == q) {
+            return root;
         }
         return root;
     }
