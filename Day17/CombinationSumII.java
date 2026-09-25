2fa9fb26f8f02fbe7f00132f264e51ec087ac2b1
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:32 +0530

    Create CombinationSumII.java

---
+++ Day17/CombinationSumII.java
@@ -0,0 +1,11 @@
+// LeetCode 40: Combination Sum II
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumII {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+}
