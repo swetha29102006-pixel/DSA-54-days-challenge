e4bb51f5e53fdeead4258c8c066934167a11912f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:35 +0530

    Create PalindromePartitioning.java

---
+++ Day17/PalindromePartitioning.java
@@ -0,0 +1,10 @@
+// LeetCode 131: Palindrome Partitioning
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        return result;
+    }
+}
