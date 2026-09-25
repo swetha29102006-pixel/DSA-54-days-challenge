ea0082f6fa8abc12ab9e5a2c768f817cfb3224ee
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:07 +0530

    Create LowestCommonAncestor.java

---
+++ Day16/LowestCommonAncestor.java
@@ -0,0 +1,20 @@
+// LeetCode 236: Lowest Common Ancestor of a Binary Tree
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return root;
+    }
+}
