9c3fe64f5459974efbf4613b66ebc0a4dc6bce93
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:12 +0530

    Update ZigzagConversion.java

--- Day14/ZigzagConversion.java
+++ Day14/ZigzagConversion.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
         return s;
     }
 }
