4b95484c017af8a5a8b48d7ad814dcc40751e355
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:09 +0530

    Update LowestCommonAncestor.java

--- Day16/LowestCommonAncestor.java
+++ Day16/LowestCommonAncestor.java
@@ -26,4 +26,13 @@ public class LowestCommonAncestor {
         }
         return left != null ? left : right;
     }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(5);
+        TreeNode q = new TreeNode(1);
+        TreeNode root = new TreeNode(3, p, q);
+        LowestCommonAncestor solution = new LowestCommonAncestor();
+        TreeNode lca = solution.lowestCommonAncestor(root, p, q);
+        System.out.println("LCA: " + (lca != null ? lca.val : "null")); // 3
+    }
 }
