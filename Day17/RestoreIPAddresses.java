58dbf4deeda5451c604461360caa5791f256b010
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:40 +0530

    Update RestoreIPAddresses.java

--- Day17/RestoreIPAddresses.java
+++ Day17/RestoreIPAddresses.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class RestoreIPAddresses {
     public List<String> restoreIpAddresses(String s) {
         List<String> result = new ArrayList<>();
+        backtrack(s, 0, 0, "", result);
         return result;
+    }
+
+    private void backtrack(String s, int index, int dots, String current, List<String> result) {
     }
 }
