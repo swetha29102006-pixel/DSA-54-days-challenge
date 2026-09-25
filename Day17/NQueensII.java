092f89f4a5c54c949d517c32de8b3f3612044b21
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:28 +0530

    Update NQueensII.java

--- Day17/NQueensII.java
+++ Day17/NQueensII.java
@@ -8,6 +8,14 @@ public class NQueensII {
         boolean[] cols = new boolean[n];
         boolean[] diag1 = new boolean[2 * n];
         boolean[] diag2 = new boolean[2 * n];
+        backtrack(0, n, cols, diag1, diag2);
         return count;
+    }
+
+    private void backtrack(int row, int n, boolean[] cols, boolean[] diag1, boolean[] diag2) {
+        if (row == n) {
+            count++;
+            return;
+        }
     }
 }
