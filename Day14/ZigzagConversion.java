400baf30b6c607c3dcee6b45b4b3f75e550fe350
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:14 +0530

    Update ZigzagConversion.java

--- Day14/ZigzagConversion.java
+++ Day14/ZigzagConversion.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1 || s.length() <= numRows) {
+        if (s == null || s.length() == 0 || numRows <= 1 || s.length() <= numRows) {
             return s;
         }
 
