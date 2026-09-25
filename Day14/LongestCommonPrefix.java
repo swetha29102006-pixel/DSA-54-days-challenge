665413b374281973265d4a646eec6200e83190a5
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:09 +0530

    Update LongestCommonPrefix.java

--- Day14/LongestCommonPrefix.java
+++ Day14/LongestCommonPrefix.java
@@ -9,6 +9,9 @@ class Solution {
         for (int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
             }
         }
 
