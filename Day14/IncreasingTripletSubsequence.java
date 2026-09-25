eda8cabe0dd907749d2677adc7937a89d9c774bd
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:15 +0530

    Update IncreasingTripletSubsequence.java

--- Day14/IncreasingTripletSubsequence.java
+++ Day14/IncreasingTripletSubsequence.java
@@ -4,7 +4,13 @@ class Solution {
         int second = Integer.MAX_VALUE;
 
         for (int num : nums) {
-            
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
         }
 
         return false;
