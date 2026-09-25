a98c32da4f66355e05f1b5f098ee2b871a362e17
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:05 +0530

    Create SymmetricTree.java

---
+++ Day16/SymmetricTree.java
@@ -0,0 +1,20 @@
+// LeetCode 101: Symmetric Tree
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return true;
+    }
+}
