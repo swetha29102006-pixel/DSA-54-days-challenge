ca212c30cc0d7c46012d23036ad81967521cf1bc
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:09 +0530

    Update LongestCommonPrefix.java

--- Day14/LongestCommonPrefix.java
+++ Day14/LongestCommonPrefix.java
@@ -7,7 +7,9 @@ class Solution {
         String prefix = strs[0];
 
         for (int i = 1; i < strs.length; i++) {
-            
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
         }
 
         return prefix;
