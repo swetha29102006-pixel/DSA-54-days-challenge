ee804c5737a99a0e9640a933b04a00570d56d430
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:30 +0530

    Create PermutationsII.java

---
+++ Day17/PermutationsII.java
@@ -0,0 +1,11 @@
+// LeetCode 47: Permutations II
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PermutationsII {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+}
