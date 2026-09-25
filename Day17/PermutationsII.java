c04f8dcb8709eec92d78c6b547741bb108fde193
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:31 +0530

    Update PermutationsII.java

--- Day17/PermutationsII.java
+++ Day17/PermutationsII.java
@@ -20,6 +20,7 @@ public class PermutationsII {
 
         for (int i = 0; i < nums.length; i++) {
             if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
         }
     }
 }
