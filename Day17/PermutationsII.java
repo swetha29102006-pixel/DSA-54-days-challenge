3647cb2c3ba2c6279f0a015527436468874c78ca
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:30 +0530

    Update PermutationsII.java

--- Day17/PermutationsII.java
+++ Day17/PermutationsII.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class PermutationsII {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
         return result;
     }
 }
