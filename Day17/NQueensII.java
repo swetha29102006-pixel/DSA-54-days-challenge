6dc9f3839e26c16a10df21c1c9359166848bd55f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:28 +0530

    Update NQueensII.java

--- Day17/NQueensII.java
+++ Day17/NQueensII.java
@@ -1,7 +1,13 @@
 // LeetCode 52: N-Queens II
 
 public class NQueensII {
+    private int count = 0;
+
     public int totalNQueens(int n) {
-        return 0;
+        count = 0;
+        boolean[] cols = new boolean[n];
+        boolean[] diag1 = new boolean[2 * n];
+        boolean[] diag2 = new boolean[2 * n];
+        return count;
     }
 }
