129c8774c70fd9b0a60436e5e1d88054b6019967
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:07 +0530

    Update SymmetricTree.java

--- Day16/SymmetricTree.java
+++ Day16/SymmetricTree.java
@@ -28,9 +28,8 @@ public class SymmetricTree {
         if (t1 == null || t2 == null) {
             return false;
         }
-        if (t1.val != t2.val) {
-            return false;
-        }
-        return isMirror(t1.left, t2.right);
+        return (t1.val == t2.val) 
+            && isMirror(t1.left, t2.right) 
+            && isMirror(t1.right, t2.left);
     }
 }
