2f0b2bc804fe5f685a12d7457d90157b446122fc
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:03 +0530

    Create InorderTraversal.java

---
+++ Day16/InorderTraversal.java
@@ -0,0 +1,23 @@
+// LeetCode 94: Binary Tree Inorder Traversal
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+}
