2d8bccf827c16c910c9fdf554c4caf739d98bcf7
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:36:06 +0530

    Update CombinationSum.java

--- Day15/CombinationSum.java
+++ Day15/CombinationSum.java
@@ -9,6 +9,11 @@ class Solution {
     }
 
     private void backtrack(List<List<Integer>> result, List<Integer> current, int[] candidates, int remain, int start) {
-
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
     }
 }
