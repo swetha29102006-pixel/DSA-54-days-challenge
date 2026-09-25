04f0b6b5fbd24e01f07a50d302912aaed73e10cb
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:01 +0530

    Update MaximumDepthOfBinaryTree.java

--- Day16/MaximumDepthOfBinaryTree.java
+++ Day16/MaximumDepthOfBinaryTree.java
@@ -15,6 +15,9 @@ class TreeNode {
 
 public class MaximumDepthOfBinaryTree {
     public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
         return 0;
     }
 }
