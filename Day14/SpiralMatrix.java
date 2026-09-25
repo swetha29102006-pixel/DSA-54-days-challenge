590905bdeeabd2dee6d4cbc6857d5424ab54ebc1
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:05 +0530

    Update SpiralMatrix.java

--- Day14/SpiralMatrix.java
+++ Day14/SpiralMatrix.java
@@ -3,6 +3,14 @@ import java.util.List;
 
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        return result;
     }
 }
