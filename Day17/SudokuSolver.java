1a5b1be43705db31064a2bced2e6c7c4fda5baa4
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:26 +0530

    Update SudokuSolver.java

--- Day17/SudokuSolver.java
+++ Day17/SudokuSolver.java
@@ -9,6 +9,8 @@ public class SudokuSolver {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                    }
                     return false;
                 }
             }
