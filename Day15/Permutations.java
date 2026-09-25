c79ff8d6ae29f3878c96d0ee83652eac3f2adca0
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:36:05 +0530

    Update Permutations.java

--- Day15/Permutations.java
+++ Day15/Permutations.java
@@ -8,23 +8,25 @@ class Solution {
         }
 
         List<List<Integer>> result = new ArrayList<>();
+        // Generate permutations via recursive backtracking
         backtrack(result, new ArrayList<>(), nums);
         return result;
     }
 
     private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums) {
+        // Base case: full permutation formed
         if (current.size() == nums.length) {
             result.add(new ArrayList<>(current));
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (current.contains(nums[i])) {
-                continue;
+        for (int num : nums) {
+            if (current.contains(num)) {
+                continue; // Skip already chosen elements
             }
-            current.add(nums[i]);
+            current.add(num);
             backtrack(result, current, nums);
-            current.remove(current.size() - 1);
+            current.remove(current.size() - 1); // Undo choice
         }
     }
 }
