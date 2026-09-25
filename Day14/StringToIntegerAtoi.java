5ca1ac55634eeecc51a0db476dec52365fae968f
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:10 +0530

    Update StringToIntegerAtoi.java

--- Day14/StringToIntegerAtoi.java
+++ Day14/StringToIntegerAtoi.java
@@ -1,5 +1,16 @@
 class Solution {
     public int myAtoi(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int index = 0;
+        int n = s.length();
+
+        while (index < n && s.charAt(index) == ' ') {
+            index++;
+        }
+
         return 0;
     }
 }
