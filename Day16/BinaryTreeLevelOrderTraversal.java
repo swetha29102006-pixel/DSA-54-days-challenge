9d3307beb70c2f5c9b9a50a9527fa8b04b616975
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:42:10 +0530

    Create BinaryTreeLevelOrderTraversal.java

---
+++ Day16/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,23 @@
+// LeetCode 102: Binary Tree Level Order Traversal
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+}
