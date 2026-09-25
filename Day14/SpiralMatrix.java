f1411b4bcd4a97bf8a0a5f2b3b02a80cdde70555
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:07 +0530

    Update SpiralMatrix.java

--- Day14/SpiralMatrix.java
+++ Day14/SpiralMatrix.java
@@ -4,24 +4,28 @@ import java.util.List;
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
-        if (matrix == null || matrix.length == 0) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return result;
         }
 
         int top = 0, bottom = matrix.length - 1;
         int left = 0, right = matrix[0].length - 1;
 
+        // Traverse in spiral layer order
         while (top <= bottom && left <= right) {
+            // Traverse top row
             for (int col = left; col <= right; col++) {
                 result.add(matrix[top][col]);
             }
             top++;
 
+            // Traverse right column
             for (int row = top; row <= bottom; row++) {
                 result.add(matrix[row][right]);
             }
             right--;
 
+            // Traverse bottom row
             if (top <= bottom) {
                 for (int col = right; col >= left; col--) {
                     result.add(matrix[bottom][col]);
@@ -29,6 +33,7 @@ class Solution {
                 bottom--;
             }
 
+            // Traverse left column
             if (left <= right) {
                 for (int row = bottom; row >= top; row--) {
                     result.add(matrix[row][left]);
