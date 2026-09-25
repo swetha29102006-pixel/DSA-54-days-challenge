90f05e4682b2762a0f4d24b3d556a5ccceb186b1
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:31:15 +0530

    Update IncreasingTripletSubsequence.java

--- Day14/IncreasingTripletSubsequence.java
+++ Day14/IncreasingTripletSubsequence.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
 
