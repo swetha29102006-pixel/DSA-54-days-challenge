e1c546342d7c4dad2dfa6491cffc00264736a86b
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:36:08 +0530

    Update LetterCombinationsOfAPhoneNumber.java

--- Day15/LetterCombinationsOfAPhoneNumber.java
+++ Day15/LetterCombinationsOfAPhoneNumber.java
@@ -2,7 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
+    private static final String[] KEYPAD = {
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+
     public List<String> letterCombinations(String digits) {
-        return new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+        return result;
     }
 }
