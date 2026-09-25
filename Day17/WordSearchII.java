d9d9806d400346e9a37597a8ff5575ab2d0fbe59
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:38 +0530

    Update WordSearchII.java

--- Day17/WordSearchII.java
+++ Day17/WordSearchII.java
@@ -2,6 +2,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    String word = null;
+}
+
 public class WordSearchII {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> result = new ArrayList<>();
