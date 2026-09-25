7608e0429eb5b95ca1284bd929aa5ce8ab6d6206
swetha29102006-pixel <swetha29102006@gmail.com>
2026-09-25 10:45:38 +0530

    Update WordSearchII.java

--- Day17/WordSearchII.java
+++ Day17/WordSearchII.java
@@ -10,6 +10,23 @@ class TrieNode {
 public class WordSearchII {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> result = new ArrayList<>();
+        TrieNode root = buildTrie(words);
         return result;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode node = root;
+            for (char c : w.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null) {
+                    node.children[idx] = new TrieNode();
+                }
+                node = node.children[idx];
+            }
+            node.word = w;
+        }
+        return root;
     }
 }
